80458230c52234f2121a1f48f6eb466becfc7aca
TaufeeqAhmad123 <130512752+TaufeeqAhmad123@users.noreply.github.com>
2026-02-19 23:34:18 +0500

    svh and fonts are added

---
+++ login/lib/core/constants/images.dart
@@ -0,0 +1,14 @@
+class AppImages{
+  //logos
+  static const String logo = "assets/svg/logo.svg";
+  static const String logo2 = "assets/svg/logo2.svg"
+  
+  //onbaording
+   static const String onbaording1 = "assets/svg/onbaording1.svg"
+   static const String onbaording2 = "assets/svg/onbaording2.svg"
+   static const String onbaording3 = "assets/svg/onbaording3.svg"
+
+   //welcome
+    static const String welcome = "assets/svg/welcome.svg"
+
+}
